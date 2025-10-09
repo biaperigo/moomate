12b806e713c9c11db7800031f8feb04e16326643
biaperigo <biaperigo01@gmail.com>
2025-10-08 22:42:59 -0300

    att

--- src/javascript/carteiraC.js
+++
@@ -1,8 +0,0 @@
-
-    const menuToggle = document.getElementById('menuToggle');
-    const navMenu = document.getElementById('navMenu');
-    menuToggle.addEventListener('click', () => navMenu.classList.toggle('show'));
-
-  document.getElementById("btnAdicionar").addEventListener("click", () => {
-    window.location.href = "adicionarsaldoC.html";
-  });
